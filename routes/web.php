5c43cad1b8c3b2dff0bbc6e206974215c0415cf5
Punan Chowdhury <punanchowdhury334@gmail.com>
2023-08-21 03:15:55 -0400

    Third commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
+
+Route::resource('listing', ListingController::class)
+  ->only(['index', 'show']);
