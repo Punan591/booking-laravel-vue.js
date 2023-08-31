d2e4ab14459b9ba887218c7dec834a90514066fe
Punan Chowdhury <punanchowdhury334@gmail.com>
2023-08-31 05:31:01 -0400

    Policy make for admin  added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,15 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //\App\Models\Listing::factory(20)->create();
+        // \App\Models\User::factory(10)->create();
 
-         \App\Models\User::factory()->create([
+        \App\Models\User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-         ]);
-         
-         \App\Models\Listing::factory(20)->create([
+            'is_admin' => true
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test2@example.com',
+        ]);
+        \App\Models\Listing::factory(10)->create([
             'by_user_id' => 1
+        ]);
+        \App\Models\Listing::factory(10)->create([
+            'by_user_id' => 2
         ]);
     }
 }
