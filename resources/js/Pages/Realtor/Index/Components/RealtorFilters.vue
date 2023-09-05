0f5e13ca2d688e7ab27180d667f93baba2b7ca89
Punan Chowdhury <punanchowdhury334@gmail.com>
2023-09-05 06:49:31 -0400

    Vue Watcher debounce lodash Implemented  added

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -5,26 +5,29 @@
           <input
             id="deleted"
             v-model="filterForm.deleted"
-            type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+            type="checkbox" 
+            class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
           />
           <label for="deleted">Deleted</label>
         </div>
       </div>
     </form>
-</template>
-
-<script setup>
-import { reactive, watch } from 'vue'
-import { Inertia } from '@inertiajs/inertia'
-const filterForm = reactive({
-  deleted: false,
-})
-// reactive / ref / computed
-watch(
-  filterForm, () => Inertia.get(
-    route('realtor.listing.index'),
-    filterForm,
-    {preserveState: true, preserveScroll: true},
-  ),
-)
-</script>
+  </template>
+  
+  <script setup>
+  import { reactive, watch } from 'vue'
+  import { Inertia } from '@inertiajs/inertia'
+  import {debounce} from 'lodash'
+  
+  const filterForm = reactive({
+    deleted: false,
+  })
+  // reactive / ref / computed
+  watch(
+    filterForm, debounce(() => Inertia.get(
+      route('realtor.listing.index'),
+      filterForm,
+      { preserveState: true, preserveScroll: true },
+    ), 1000),
+  )
+  </script>
