9400c0541ea9be3585069c55af9149bd84a7b440
Punan Chowdhury <punanchowdhury334@gmail.com>
2023-09-10 11:34:58 -0400

    Image uploading vue image file submit image fetch to show in frontend completed

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\UserAccountController;
 use App\Http\Controllers\RealtorListingController;
+use App\Http\Controllers\RealtorListingImageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -48,4 +49,7 @@ Route::delete('logout', [AuthController::class, 'destroy'])
     Route::resource('listing', RealtorListingController::class)
       ->only(['index', 'destroy', 'edit', 'update', 'create', 'store'])
       ->withTrashed();
+
+    Route::resource('listing.image', RealtorListingImageController::class)
+      ->only(['create', 'store']);
   });
